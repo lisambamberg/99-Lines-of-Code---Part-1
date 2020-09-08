4425969bcae5fd35fed705b4b50a36b8c5816bc0
lisambamberg <lisambamberg@gmail.com>
2020-09-08 11:11:21 -0500

    Added if condition for case where the count = 2

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ let friends = ["Sarah", "Eden", "Lauren", "Rachel", "Sara"];
 
 for (var i = 0; i < friends.length; i++) {
     for (var j = 99; j > 1; j--) {
-        console.log(`${j} lines of code in the file, lines of code; ${friends[i]} strikes one out, clears it all out, ${j - 1} lines of code in the file`);
-    } console.log(`1 line of code in the file, 1 line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file`);
+        if (j > 2) {
+            console.log(`${j} lines of code in the file, ${j} lines of code; ${friends[i]} strikes one out, clears it all out, ${j - 1} lines of code in the file.`);
+        }
+        if (j == 2) {
+            console.log(`2 lines of code in the file, 2 lines of code; ${friends[i]} strikes one out, clears it all out, 1 line of code in the file.`);
+        }
+    } console.log(`1 line of code in the file, 1 line of code; ${friends[i]} strikes one out, clears it all out, no more lines of code in the file!`);
 }
